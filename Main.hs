0e3b227c9752419f0067c1c3845e13bca37a3a2c
Arianna Masciolini <uzkamascio@gmail.com>
2023-01-10 10:35:00 +0100

    fix indentation

--- Main.hs
+++ Main.hs
@@ -13,29 +13,29 @@ import ConceptAlignment
 import L2UD
 
 main = do
-    argv <- getArgs
-    let (flags,args) = parseArgv argv usage opts
-    if Help `elem` flags || length args < 3 
-        then putStrLn $ usageInfo usage opts
+  argv <- getArgs
+  let (flags,args) = parseArgv argv usage opts
+  if Help `elem` flags || length args < 3 
+    then putStrLn $ usageInfo usage opts
+    else do
+      l1Sentences <- parseUDFile (args !! 0)
+      l2Sentences <- parseUDFile (args !! 1)
+      let alignments = defaultAlign l1Sentences l2Sentences 
+      isFile <- doesFileExist $ args !! 2
+      queries <- if length args == 3 && isFile
+        then do
+          content <- readFile $ args !! 2
+          return $ lines content
+        else return $ drop 2 args
+      let errorPatterns = map parseQuery queries
+      let matches = concatMap (queryL1L2treebank alignments) errorPatterns
+      if Linearize `elem` flags
+        then mapM_ (putStrLn . prettyPrintAlignment) matches
         else do
-            l1Sentences <- parseUDFile (args !! 0)
-            l2Sentences <- parseUDFile (args !! 1)
-            let alignments = defaultAlign l1Sentences l2Sentences 
-            isFile <- doesFileExist $ args !! 2
-            queries <- if length args == 3 && isFile
-                then do
-                    content <- readFile $ args !! 2
-                    return $ lines content
-                else return $ drop 2 args
-            let errorPatterns = map parseQuery queries
-            let matches = concatMap (queryL1L2treebank alignments) errorPatterns
-            if Linearize `elem` flags
-                then mapM_ (putStrLn . prettyPrintAlignment) matches
-                else do
-                    createDirectoryIfMissing True "out"
-                    let (l1s, l2s) = unzip $ map alignment2sentencePair matches
-                    writeFile "out/L1.conllu" (unlines $ [prUDSentence n s | (n, s) <- [1 .. ] `zip` l1s])
-                    writeFile "out/L2.conllu" (unlines $ [prUDSentence n s | (n, s) <- [1 .. ] `zip` l2s])
+          createDirectoryIfMissing True "out"
+          let (l1s, l2s) = unzip $ map alignment2sentencePair matches
+          writeFile "out/L1.conllu" (unlines $ [prUDSentence n s | (n, s) <- [1 .. ] `zip` l1s])
+          writeFile "out/L2.conllu" (unlines $ [prUDSentence n s | (n, s) <- [1 .. ] `zip` l2s])
 
 -- COMMAND LINE OPTIONS PARSING
   
