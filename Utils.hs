9635e97165cae07df8883376c98c3ace5b9ff625
Arianna Masciolini <uzkamascio@gmail.com>
2023-01-12 21:22:12 +0100

    fix #5

--- Utils.hs
+++ Utils.hs
@@ -1,6 +1,10 @@
 module Utils where
 
+import RTree
 import UDConcepts
 
 udTree2adjustedSentence :: UDTree -> UDSentence
 udTree2adjustedSentence = adjustUDIds . udTree2sentence . createRoot
+
+rootID :: UDTree -> UDId
+rootID (RTree n _) = udID n
