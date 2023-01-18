60b5e63f1e98636c6cdfb7314055b0d0e220652d
Arianna Masciolini <uzkamascio@gmail.com>
2023-01-18 21:18:59 +0100

    let's say alignment is good for now

--- Align.hs
+++ Align.hs
@@ -10,56 +10,38 @@ type Alignment = (UDTree,UDTree)
 
 -- ALIGNMENT CRITERIA FOR L1-L2 TREEBANKS
   
--- | Convert a comparison function into a full-blown Criterion
-mkCriterion :: (UDTree -> UDTree -> Bool) -> Criterion
-mkCriterion f = C f (singleton UNKNOWN) False False 
-
 -- | List of criteria used by align, sorted by priority
 criteria :: [Criterion]
-criteria = [udpos, ud, pos]
+criteria = [udposlemma, udpos, lemma, ud, pos]
 
 {- Functions used in criteria -}
-
--- | Original basic criterion: matching root UD labels 
-udMatch :: UDTree -> UDTree -> Bool
-(RTree n ts) `udMatch` (RTree m us) = udSimpleDEPREL n == udSimpleDEPREL m
-
--- | POS-equivalence
-posEquiv :: UDTree -> UDTree -> Bool
-t1 `posEquiv` t2 = (not . null) ct1 && (ct1 == ct2)
-  where (ct1, ct2) = (contentTags t1, contentTags t2)
   
-ud, pos, udpos :: Criterion
-ud = C udMatch (singleton UD) True False
+ud, pos, lemma, udpos, udposlemma :: Criterion
+ud = C sameSimpleDeprel (singleton UD) True False
 pos = C posEquiv (singleton POS) True False 
+lemma = C lemmaEquiv (singleton LEMMA) True False
 udpos = 
-  C (\t u -> udMatch t u && posEquiv t u) (fromList [UD,POS]) True True
-
--- | Exact same root token
-sameToken :: UDTree -> UDTree -> Bool
-sameToken (RTree n _) (RTree m _) = n == m
+  C (\t u -> sameSimpleDeprel t u && posEquiv t u) (fromList [UD,POS]) True True
+udposlemma = C (\t u -> sameSimpleDeprel t u && posEquiv t u && lemmaEquiv t u) (fromList [UD,POS, LEMMA]) True True
 
 -- | Same word from
 sameForm :: UDTree -> UDTree -> Bool
 sameForm (RTree n _) (RTree m _) = udFORM n == udFORM m
 
--- | Same lemma
-sameLemma :: UDTree -> UDTree -> Bool
-sameLemma (RTree n _) (RTree m _) = udLEMMA n == udLEMMA m
-
--- | Same dependency relation
-sameDeprel :: UDTree -> UDTree -> Bool
-sameDeprel (RTree n _) (RTree m _) = udDEPREL n == udDEPREL m
+-- | Lemma-equivalence
+lemmaEquiv :: UDTree -> UDTree -> Bool
+t1 `lemmaEquiv` t2 = (not . null) l1s && (l1s == l2s)
+  where (l1s, l2s) = (contentLemmas t1, contentLemmas t2)
 
 -- | Same dependency relation (ignoring subtypes)
 sameSimpleDeprel :: UDTree -> UDTree -> Bool
 sameSimpleDeprel (RTree n _) (RTree m _) = 
   udSimpleDEPREL n == udSimpleDEPREL m
 
--- | Same root UPOS  
--- (maybe use bag of contentTags for bag of subtree POSs?)
-sameUPOS :: UDTree -> UDTree -> Bool
-sameUPOS (RTree n _) (RTree m _) = udUPOS n == udUPOS n
+-- | POS-equivalence
+posEquiv :: UDTree -> UDTree -> Bool
+t1 `posEquiv` t2 = (not . null) ct1 && (ct1 == ct2)
+  where (ct1,ct2) = (contentTags t1, contentTags t2)
 
 -- | alignSent wrapper to align with default "optional arguments" and return
 -- pairs of alignment rather than the idiotic Alignment data type I for some
