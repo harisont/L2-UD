95b8e28a15659151f34ac6eb17a91c15cfc1b1c6
Arianna Masciolini <uzkamascio@gmail.com>
2023-10-11 17:38:52 +0200

    tried to load files but paths are empty?

--- src/GUI.hs
+++ src/GUI.hs
@@ -7,6 +7,8 @@ Stability   : experimental
 module GUI where 
 import Graphics.UI.Threepenny.Core
 import qualified Graphics.UI.Threepenny as UI
+import UDConcepts
+import Utils.Output (lin)
 
 main :: IO ()
 main = do
@@ -16,8 +18,8 @@ main = do
     } setup
 
 setup :: Window -> UI ()
-setup w = do
-  return w # set UI.title "L2-UD"
+setup window = do
+  return window # set UI.title "L2-UD"
 
   l1Input <- UI.input
   element l1Input # set
@@ -45,12 +47,33 @@ setup w = do
   searchButton <- UI.button
   element searchButton # set UI.text "search"
 
-  getBody w #+ [element l1Input, 
+  getBody window #+ [element l1Input, 
                 element l2Input,
                 element loadButton,
                 element break, 
                 element queryInput, 
                 element searchButton] 
+  
+  -- !?!?!?!?!?!?!?
+  l1Path <- get value l1Input 
+  l2Path <- get value l2Input
 
+  on UI.click loadButton $ const $ loadTreebank window l1Path l2Path
   on UI.click searchButton $ undefined
-    
+
+-- | Init table with L1-L2 treebank
+loadTreebank :: Window -> FilePath -> FilePath -> UI Element
+loadTreebank window path1 path2 = do
+  -- TODO: check if the paths are valid
+
+  l1Sents <- liftIO $ parseUDFile path1
+  l2Sents <- liftIO $ parseUDFile path2 
+  
+  table <- buildTable window (map lin l1Sents) (map lin l2Sents)
+  getBody window #+ [element table]
+
+buildTable :: Window -> [String] -> [String] -> UI Element
+buildTable window l1Data l2Data = do 
+  cells <- mapM (mapM (return . string)) (zipWith (\s1 s2 -> [s1,s2]) l1Data l2Data)
+  table <- UI.grid cells
+  return table
