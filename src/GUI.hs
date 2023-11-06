f9ce71d4e777e2e983b9c5d83e087413342d35bf
Arianna Masciolini <uzkamascio@gmail.com>
2023-11-06 18:47:40 +0100

    improved GUI (#39)
    * start gui with threepenny
    
    * Electron stuff
    
    * Revert "Electron stuff"
    
    This reverts commit e32672439a0551e46fd07d16b1a93dbd48864d4f.
    
    * essential ui elems
    
    * tried to load files but paths are empty?
    
    * successfully init table
    
    * very very minimal GUI for match command
    
    * small but fundamental graphical improvements
    
    * try try
    
    * Revert "try try"
    
    This reverts commit 6af4142f105b1e5abb1d32a07ad0c3b5a806a807.
    
    * add Match to GUI other-modules
    
    * implement subtree highlighting
    
    * ignore tmp files
    
    * implement replacement rule
    
    * cleaner code for building text inputs
    
    * add conllu mode
    
    * Create win_notes.md
    
    Add notes for compiling on windows
    
    * change missingh version so that stuff compiles on win too
    
    * update win instructions
    
    * move css rules to hs

--- src/GUI.hs
+++ src/GUI.hs
@@ -5,6 +5,7 @@ Stability   : experimental
 -}
 
 module GUI where 
+import Data.Maybe
 import System.Directory
 import Graphics.UI.Threepenny.Core
 import qualified Graphics.UI.Threepenny as UI
@@ -16,6 +17,8 @@ import Align
 import Match
 import Errors
 
+data Mode = Text | CoNNLU deriving (Eq, Show)
+
 main :: IO ()
 main = do
   startGUI defaultConfig
@@ -26,47 +29,53 @@ main = do
 setup :: Window -> UI ()
 setup window = do
   return window # set UI.title "L2-UD"
-  UI.addStyleSheet window "style.css"
 
-  l1Input <- UI.input
-  element l1Input # set (UI.attr "placeholder") ("path to L1 treebank")
-  element l1Input # set (UI.attr "class") "path"
-  markRight l1Input
+  l1Input <- buildTextInput "path to L1 treebank" "path"
+  element l1Input # set UI.style [("width","49.4%")]
+  l2Input <- buildTextInput "path to L2 treebank" "path"
+  element l2Input # set UI.style [("width","49.4%")]
 
-  l2Input <- UI.input
-  element l2Input # set (UI.attr "placeholder") ("path to L2 treebank")
-  element l2Input # set (UI.attr "class") "path"
-  markRight l2Input
-
-  queryInput <- UI.input
-  element queryInput # set
-    (UI.attr "placeholder")
-    ("L1-L2 or single-language (matched on L2) query")
-  element queryInput # set (UI.attr "id") "query"
-  markRight queryInput
+  queryInput <- buildTextInput
+                  "L1-L2 or single-language (matched on L2) query"
+                  "query"
+  element queryInput # set UI.style [("width","99.2%")]
 
   searchButton <- UI.button
   element searchButton # set UI.text "search"
+  element searchButton # set UI.style [
+      ("margin","1%")
+    , ("font-family", "arial, sans-serif")
+    ]
 
-  replacementInput <- UI.input
-  element replacementInput # set
-    (UI.attr "placeholder")
-    ("additional replacement rule (optional)")
-  element replacementInput # set (UI.attr "id") "replacement"
-  markRight replacementInput
+  replacementInput <- buildTextInput 
+                        "additional replacement rule (optional)"
+                        "replacement"
+  element replacementInput # set UI.style [("width","99.2%")]
+
+  modeSpan <- string "Mode: "
+  
+  textMode <- buildMode "text" True
+  conlluMode <- buildMode "CoNNL-U" False
 
   getBody window #+ [
-                element l1Input, 
-                element l2Input,
-                element queryInput, 
-                element searchButton,
-                element replacementInput] 
+                element l1Input
+              , element l2Input
+              , element queryInput 
+              , element replacementInput
+              , element modeSpan
+              , element textMode
+              , element conlluMode
+              , element searchButton
+                ] 
   
   on UI.click searchButton $ const $ do
     l1Path <- get value l1Input 
     l2Path <- get value l2Input
     queryTxt <- get value queryInput
     replacementTxt <- get value replacementInput
+    textModeButton <- getElementById window "text"
+    textModeValue <- get UI.checked (fromJust $ textModeButton)
+    let mode = if textModeValue then Text else CoNNLU
 
     l1Exists <- liftIO $ doesFileExist l1Path
     l2Exists <- liftIO $ doesFileExist l2Path
@@ -102,7 +111,21 @@ setup window = do
                             (\(s,es) -> 
                               (s,map (applyReplacement replacement) es))
                             matches
-            let (l1Col,l2Col) = unzip $ concatMap (\((s1,s2),ms) -> map (\(m1,m2) -> (highlin s1 (udTree2sentence m1) HTML, highlin s2 (udTree2sentence m2) HTML)) ms) matches'
+            let (l1Col,l2Col) = unzip $ concatMap 
+                  (\((s1,s2),ms) -> 
+                    map 
+                      (\(m1,m2) -> 
+                        let m1' = udTree2sentence (adjustRootAndPositions m1)
+                            m2' = udTree2sentence (adjustRootAndPositions m2)
+                        in ((if mode == Text 
+                            then highlin s1 (udTree2sentence m1) HTML
+                            else prReducedUDSentence "xxxxxxxx" m1', 
+                          if mode == Text 
+                            then highlin s2 (udTree2sentence m2) HTML
+                            else prReducedUDSentence "xxxxxxxx" m2')
+                        )) 
+                      ms) 
+                  matches'
             table <- buildTable window l1Col l2Col 
             destroyTables window
             getBody window #+ [element table]
@@ -113,16 +136,57 @@ setup window = do
   where applyReplacement r (e1,e2) = 
           (fst $ replacementsWithUDPattern r e1,
            fst $ replacementsWithUDPattern r e2)
-    
+
+type Placeholder = String
+type Class = String
+
+buildTextInput :: Placeholder -> Class -> UI Element
+buildTextInput p c = do
+  input <- UI.input
+  element input # set (UI.attr "placeholder") p
+  element input # set (UI.attr "class") c
+  element input # set UI.style [("font-family", "arial, sans-serif")]
+  markRight input
+  return input
+
+buildMode :: String -> Bool -> UI Element
+buildMode mode checked = do
+  span <- UI.span
+  radioButton <- UI.input
+  label <- UI.label
+  element radioButton # set UI.type_ "radio"
+  element radioButton # set UI.name "mode"
+  element radioButton # set UI.id_ mode
+  element radioButton # set UI.checked checked
+  element label # set UI.type_ "label"
+  element label # set UI.text mode
+  element label # set UI.for mode
+  element span # set children [radioButton, label]
+  element span # set UI.style [("font-family", "arial, sans-serif")]
+  return span
+      
 buildTable :: Window -> [String] -> [String] -> UI Element
 buildTable window l1Data l2Data = do 
   cells <- mapM 
-            (mapM (return . (\htmlText -> do
-              div <- UI.div
-              element div # set html htmlText
-              return div)))
-            (zipWith (\s1 s2 -> [s1,s2]) l1Data l2Data)
+            (\(n,row) -> mapM 
+              (return . (\htmlText -> do
+                div <- UI.div
+                element div # set html htmlText
+                element div # set UI.style [
+                    ("text-align", "left")
+                  , ("padding", "8px")
+                  , ("white-space", "pre-wrap")
+                  , ("background-color", if even n then "#dddddd" else "#ffffff")
+                  ]
+                return div))
+              row)
+            ([1..] `zip` zipWith (\s1 s2 -> [s1,s2]) l1Data l2Data)
   table <- UI.grid cells
+  element table # set UI.style [
+      ("width","100%")
+    , ("table-layout","fixed")
+    , ("font-family", "arial, sans-serif")
+    ]
   return table
 
 destroyTables :: Window -> UI ()
